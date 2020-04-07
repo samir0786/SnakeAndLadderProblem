9a3c401d04ccbde4beb6c25ac556321d8ef2a073
samir0786 <samirsiescom@gmail.com>
2020-04-07 16:02:07 +0300

    ADDED: Function To Check noPlay,Ladder or Snake

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -1,7 +1,33 @@
 #!/bin/bash -x
-echo "Welcome to SnakeAndLadder Problem"
+echo "Welcome to Snake And Ladder Simulator"
 
-#!Initialize veriables
+#!Initialize variables & constants
 playerPosition=0
-dieValue=$((RANDOM%6+1))
-echo $dieValue
+NO_PLAY=0
+LADDER=1
+SNAKE=2
+
+#!Function To set playerPosition according to playing Options like NO_Play or Snake or Ladder
+function setPlayerMoves() {
+
+	dieValue=$((RANDOM % 6 + 1))
+	playingOptions=$((RANDOM % 3))
+
+	#! Increment player Position according to $playingOptions
+
+	case $playingOptions in
+		$NO_PLAY)
+			playerPosition=$playerPosition
+			;;
+		$LADDER)
+			playerPosition=$((playerPosition + dieValue))
+			;;
+		$SNAKE)
+			playerPosition=$((playerPosition - dieValue))
+			;;
+	esac
+echo "Player Position : "$playerPosition
+}
+
+#!Start game
+setPlayerMoves
