155d8167657170b53db6ba74855fd4839eacc0a0
samir0786 <samirsiescom@gmail.com>
2020-04-08 10:21:10 +0300

    ADDED: Function To Check NoOF Times dieRoll and Player Position

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -3,15 +3,18 @@ echo "Welcome to Snake And Ladder Simulator"
 
 #!Initialize variables & constants
 playerPosition=0
+diceRoll=0
 NO_PLAY=0
 LADDER=1
 SNAKE=2
+declare -A gamePlay
 
 #!Function To set playerPosition according to playing Options like NO_Play or Snake or Ladder
 function setPlayerMoves() {
 
 	dieValue=$((RANDOM % 6 + 1))
 	playingOptions=$((RANDOM % 3))
+	((diceRoll++))
 
 	#! Increment player Position according to $playingOptions
 	case $playingOptions in
@@ -27,6 +30,7 @@ function setPlayerMoves() {
 	esac
 		#!Reset player Position if position is not between 0 to 100
 		resetPosition
+		gamePlay[DiceRoll:$diceRoll]=$playerPosition
 
 }
 function exactWinPosition() {
